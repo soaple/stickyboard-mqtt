f517140e54e388a610c8917ae459f11a38625ee2
Soaple <inje@soaple.io>
2020-09-08 18:12:15 +0900

    Loading animation updated, code refactoring, and version updated to v0.1.1

--- src/MqttComponent.js
+++ src/MqttComponent.js
@@ -3,26 +3,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Textfit } from 'react-textfit';
-import styled, { keyframes, css }  from 'styled-components';
-import ClipLoader from "react-spinners/ClipLoader";
+import styled, { keyframes } from 'styled-components';
+import PuffLoader from 'react-spinners/PuffLoader';
 
 import MqttClient from './MqttClient';
 
-const maxFlicker = keyframes`
+const maxFlickerAnim = keyframes`
     0% {
-      color: red;
+        color: #ff4747;
     }
     50% {
-      color: #ffffff;
-    }`
+        color: #ffffff;
+    }`;
 
-const minFlicker = keyframes`
+const minFlickerAnim = keyframes`
     0% {
-      color: #0000ff;
+        color: #3b59f5;
     }
     50% {
-      color: #ffffff;
-    }`
+        color: #ffffff;
+    }`;
 
 const Wrapper = styled.div`
     height: 100%;
@@ -42,6 +42,13 @@ const ServerUrlText = styled.div`
     text-align: left;
 `;
 
+const TopicText = styled.div`
+    width: 100%;
+    font-size: 12px;
+    color: #ffffff;
+    text-align: left;
+`;
+
 const MessageTextfit = styled(Textfit)`
     width: 100%;
     flex: 1;
@@ -50,11 +57,14 @@ const MessageTextfit = styled(Textfit)`
     align-items: center;
     font-weight: bold;
     color: #ffffff;
-    ${props =>
-      props.active &&
-      css`
-      animation: ${props.active == 'max' ? maxFlicker : minFlicker} 1s 1s infinite linear alternate;
-    `};
+    ${(props) =>
+        props.active &&
+        `
+            animation: ${
+                props.active === 'max' ? maxFlickerAnim : minFlickerAnim
+            } 1s 1s
+                infinite linear alternate;
+        `};
 `;
 
 const LastUpdatedText = styled.div`
@@ -69,7 +79,7 @@ class MqttComponent extends React.Component {
         super(props);
 
         this.state = {
-            message: '',
+            message: null,
             lastUpdated: null,
         };
     }
@@ -97,7 +107,6 @@ class MqttComponent extends React.Component {
                 message: message.toString(),
                 lastUpdated: new Date(),
             });
-            
         });
     }
 
@@ -105,62 +114,62 @@ class MqttComponent extends React.Component {
         MqttClient.end();
     }
 
-    getActiveType(){
-      const { message } = this.state;
-      const { maxValue, minValue} = this.props;
-      let activeType = "";
+    getActiveType() {
+        const { message } = this.state;
+        const { maxValue, minValue } = this.props;
 
-      if(message.slice(0, -3) > maxValue){
-        activeType = "max";
-      }else if(message.slice(0, -3) < minValue){
-        activeType = "min";
-      }
-      return activeType;
+        let activeType = null;
+        if (message > maxValue) {
+            activeType = 'max';
+        } else if (message < minValue) {
+            activeType = 'min';
+        }
+        return activeType;
     }
 
     render() {
         const { message, lastUpdated } = this.state;
         const { serverUrl, subscribeTopic } = this.props;
+
+        if (!message) {
+            return (
+                <Wrapper>
+                    <PuffLoader size={56} color={'#ffffff'} loading={true} />
+                </Wrapper>
+            );
+        }
+
         const activeType = this.getActiveType();
 
-        if (message == ''){
-          return (
+        return (
             <Wrapper>
-              <ClipLoader
-                size={80}
-                color={"#39C2F7"}/>
-            </Wrapper>
-          );
-        }else {
-          return (
-            <Wrapper>
-              <ServerUrlText>{`Server: ${serverUrl}`}</ServerUrlText>
-              <ServerUrlText>{`Topic: ${subscribeTopic}`}</ServerUrlText>
+                <ServerUrlText>{`Server: ${serverUrl}`}</ServerUrlText>
+                <TopicText>{`Topic: ${subscribeTopic}`}</TopicText>
 
-              <MessageTextfit
-                mode="single"
-                min={16}
-                max={224}
-                forceSingleModeWidth={false}
-                active={activeType}
-                >
-                {message}
-              </MessageTextfit>
-              
-              {lastUpdated && (
-                  <LastUpdatedText>
-                      {`Last updated: ${lastUpdated.toLocaleString()}`}
-                  </LastUpdatedText>
-              )}
+                <MessageTextfit
+                    mode="single"
+                    min={16}
+                    max={224}
+                    forceSingleModeWidth={false}
+                    active={activeType}>
+                    {message}
+                </MessageTextfit>
+
+                {lastUpdated && (
+                    <LastUpdatedText>
+                        {`Last updated: ${lastUpdated.toLocaleString()}`}
+                    </LastUpdatedText>
+                )}
             </Wrapper>
-          );            
-        }
+        );
     }
 }
 
 MqttComponent.propTypes = {
     serverUrl: PropTypes.string.isRequired,
     subscribeTopic: PropTypes.string.isRequired,
+    minValue: PropTypes.string,
+    maxValue: PropTypes.string,
 };
 
 export default MqttComponent;
